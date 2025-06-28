a4189eae551d0cf0629674753cfccdc886b3992f
thekidmellow <thekidmellow@gmail.com>
2025-06-28 17:49:45 +0200

    Implement method to create JSON file if it does not exist

--- personal-expense-tracker/data_handler.py
+++ personal-expense-tracker/data_handler.py
@@ -10,3 +10,9 @@ class DataHandler:
     def __init__(self, filename: str = "expenses.json"):
         self.filename = filename
         self.ensure_file_exists()
+
+    def ensure_file_exists(self) -> None:
+        """Create the data file if it doesn't exist"""
+        if not os.path.exists(self.filename):
+            with open(self.filename, 'w') as file:
+                json.dump([], file)
