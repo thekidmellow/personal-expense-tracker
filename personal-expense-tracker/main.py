caefdc469c28921a88bf9cf5759721449cc97184
thekidmellow <thekidmellow@gmail.com>
2025-06-30 10:46:55 +0200

    Add script entry point and imports for CLI expense tracker

---
+++ personal-expense-tracker/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Personal Expense Tracker
+A command-line application to manage personal expenses
+"""
+
+import sys
+from typing import Optional
+from expense_manager import ExpenseManager
