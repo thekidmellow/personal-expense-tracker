9df302b36f4774a51acf1a2cc2c622e973d262a5
thekidmellow <thekidmellow@gmail.com>
2025-06-30 10:48:21 +0200

    Create ExpenseTrackerApp class and initialize ExpenseManager

--- personal-expense-tracker/main.py
+++ personal-expense-tracker/main.py
@@ -7,3 +7,10 @@ A command-line application to manage personal expenses
 import sys
 from typing import Optional
 from expense_manager import ExpenseManager
+
+
+class ExpenseTrackerApp:
+    """Main application class for the expense tracker"""
+
+    def __init__(self):
+        self.expense_manager = ExpenseManager()
